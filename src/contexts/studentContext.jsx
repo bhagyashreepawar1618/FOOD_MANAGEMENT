17cba62c3c8cc3937bfe778c2f9804aa9856fd57
bhagyashreepawar1618 <bhagyashreepawar1618@gmail.com>
2026-03-01 16:41:56 +0530

    cretaed StudentContext to store Student profile information

---
+++ src/contexts/studentContext.jsx
@@ -0,0 +1,22 @@
+import { createContext, useContext, useState } from "react";
+
+const StudentContext = createContext();
+
+export const StudentProvider = ({ children }) => {
+  const [student, setStudent] = useState();
+  const [studentToken, setStudentToken] = useState();
+
+  const value = {
+    student,
+    setStudent,
+    studentToken,
+    setStudentToken,
+  };
+  return (
+    <StudentContext.Provider value={value}>{children}</StudentContext.Provider>
+  );
+};
+
+export const useStudent = () => {
+  return useContext(StudentContext);
+};
