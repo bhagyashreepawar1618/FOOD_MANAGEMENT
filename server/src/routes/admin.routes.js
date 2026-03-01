70a2a1fa8397b172cbfa3092a1de242813177f53
bhagyashreepawar1618 <bhagyashreepawar1618@gmail.com>
2026-03-01 23:54:30 +0530

    added setmenu route

--- server/src/routes/admin.routes.js
+++ server/src/routes/admin.routes.js
@@ -1,9 +1,14 @@
-import { registerAdmin, loginAdmin } from "../controllers/admin.js";
+import {
+  registerAdmin,
+  loginAdmin,
+  setMenuData,
+} from "../controllers/admin.js";
 import { Router } from "express";
 
 const router = Router();
 
 router.route("/register").post(registerAdmin);
 router.route("/login").post(loginAdmin);
+router.route("/setmenu").post(setMenuData);
 
 export default router;
