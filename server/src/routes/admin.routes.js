5de01b9596e2870c37d9b574b7ea121ca95707a6
bhagyashreepawar1618 <bhagyashreepawar1618@gmail.com>
2026-03-02 18:10:52 +0530

    added update admin profile route

--- server/src/routes/admin.routes.js
+++ server/src/routes/admin.routes.js
@@ -2,13 +2,16 @@ import {
   registerAdmin,
   loginAdmin,
   setMenuData,
+  updateAdminProfile,
 } from "../controllers/admin.js";
 import { Router } from "express";
+import { verifyJWT } from "../middlewares/authAdmin.middleware.js";
 
 const router = Router();
 
 router.route("/register").post(registerAdmin);
 router.route("/login").post(loginAdmin);
 router.route("/setmenu").post(setMenuData);
+router.route("/update-profile").post(verifyJWT, updateAdminProfile);
 
 export default router;
